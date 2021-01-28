3275f7c365019d891166af9d8d40bf6ea25317b2
Victor Santos <victorhf@pop-os.localdomain>
2021-01-28 00:56:53 -0300

    algumas alteraçoes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-
 
 /* const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -26,9 +29,16 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Movie Quiz</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
 
           <Widget.Header>
@@ -36,25 +46,46 @@ export default function Home() {
           </Widget.Header>
 
           <Widget.Content>
-            <p>TESTANDO ESSA BAGAÇA...</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('TESTANDO');
+
+              //  Router manda para próxima página
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                  // console.log(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu Nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
 
         </Widget>
 
         <Widget>
-          
+
           <Widget.Header>
             <h1>SERÁ QUE FUNCIONA ISSO?</h1>
           </Widget.Header>
-          
+
           <Widget.Content>
             <p>NÃO SEI PORRA</p>
           </Widget.Content>
 
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/ViictorHFz"/>
+      <GitHubCorner projectUrl="https://github.com/ViictorHFz" />
     </QuizBackground>
-  ) 
+  );
 }
